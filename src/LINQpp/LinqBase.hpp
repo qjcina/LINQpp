37be506344dc3091aaa7a60455f5a79c2f9cd165
Marcin Pawlikowski <kojoten95@gmail.com>
2021-02-20 17:17:26 +0100

    dude...

--- src/LINQpp/LinqBase.hpp
+++ src/LINQpp/LinqBase.hpp
@@ -51,7 +51,7 @@ namespace linq
          */
         template <typename LambdaType,
                   typename LambdaReturnType = std::result_of_t<LambdaType(ElementType)>,
-                  typename ReturnContainerType = std::vector<LambdaReturnType>,
+                  typename ReturnContainerType = std::list<LambdaReturnType>,
                   typename ReturnLinqType = LinqObjectBase<typename ReturnContainerType>>
         ReturnLinqType select(const LambdaType &builder) const;
 
